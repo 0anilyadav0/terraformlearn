0fc4159c02246efe91aa0ed7d34f30766b1280d1
Anil Yadav <0anilyadav0@gmail.com>
2022-01-08 20:28:08 +0530

    upload

--- mgmttf/resourcegroup.tf
+++ mgmttf/resourcegroup.tf
@@ -1,4 +1,4 @@
-#terraform import azurerm_resource_group.rg01 /subscriptions/b9e0d361-8c90-41bc-9099-a1f575078fea/resourceGroups/rg01
+#terraform import azurerm_resource_group.rg01 /subscriptions/b9e0d361-xxxxxx575078fea/resourceGroups/rg01
 resource "azurerm_resource_group" "rg01" {
   name     = "tfrg01"
   location = "eastus"
